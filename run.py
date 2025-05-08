946996bd00e1eb096d3a4adc3a3fc7d5c7b6c768
samikshyadhamala <samikshya.dhamala88@gmail.com>
2025-05-08 12:06:10 +0545

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+# from backend import create_app
+
+# print("Creating app...")  # Add this line
+# app = create_app()
+
+# if __name__ == "__main__":
+#     print("Running app...")  # Add this too
+#     app.run(debug=True)
+
